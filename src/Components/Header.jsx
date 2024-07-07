4e88e4f10b65abda3cbe9f21de6a056cfd39aa17
Amelss <ameleyka@gmail.com>
2024-07-07 19:29:18 +0100

    Adjusting navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,87 +1,82 @@
 import { slide as Menu } from "react-burger-menu";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "../index.css"
-
+import "../index.css";
 
 export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
 
-    // const [navBarOpen, setNavBarOpen] = useState(false);
-    // const [openMenu, setOpenMenu] = useState(false);
+  const closeMenu = () => {
+    setOpenMenu(false);
+  };
 
-    // const handleOpen = () => {
-    //   setNavBarOpen(!navBarOpen);
-    // };
-
-    // const showMenu = () => {
-    //   setOpenMenu(!openMenu);
-    // };
   return (
-    <div className="p-4">
+    <div className="xl:p-4">
       <div className="xl:hidden">
         <div>
           <h1 className="font-bold text-1xl">BLUESPACE</h1>
         </div>
         <div>
-          <Menu>
-            <Link to={"/"} className="menu-item">
+          <Menu
+            right
+            isOpen={openMenu}
+            onStateChange={({ isOpen }) => setOpenMenu(isOpen)}
+          >
+            {/* <Link to="/" className="menu-item" onClick={closeMenu}>
               Home
-            </Link>
-            <Link to={"/Categories"} className="menu-item">
+            </Link> */}
+            <Link to="/Categories" className="menu-item" onClick={closeMenu}>
               Categories
             </Link>
-            <Link to={"/Help"} className="menu-item">
+            <Link to="/Help" className="menu-item" onClick={closeMenu}>
               Help
             </Link>
-            <Link to={"/Contact"} className="menu-item">
+            <Link to="/Contact" className="menu-item" onClick={closeMenu}>
               Contact
             </Link>
-            <Link to={"/Cart"} className="menu-item">
+            <Link to="/Cart" className="menu-item" onClick={closeMenu}>
               Cart
             </Link>
-            <Link to={"/"} className="menu-item">
+            <Link to="/" className="menu-item" onClick={closeMenu}>
               Join Bluespace
             </Link>
           </Menu>
         </div>
       </div>
-      <div className="hidden xl:flex">
-
-        
-        
-</div>
-      <div className="hidden xl:flex justify-between items-center ">
-        
-            <div>
-          <h1>BLUESPACE</h1>
+      <div className="hidden xl:flex justify-between items-center">
+        <div className="bg-white w-full py-3 px-2">
+          <h1 className="ml-3 font-bold">BLUESPACE</h1>
         </div>
-        <div className="flex justify-between ">
-          <div className="bg-white" >
-          <Link to={"/"} className="menu-item">
-            Home
-          </Link>
-          <Link to={"/Categories"} className="menu-item">
-            Categories
-          </Link>
-          <Link to={"/Help"} className="menu-item">
-            Help
-          </Link>
-          <Link to={"/Contact"} className="menu-item">
-            Contact
-          </Link>
-        </div>
+        <div className="flex items-center">
+          <div className="bg-white  py-3 px-6">
+            {/* <Link to="/" className="">
+              Home
+            </Link> */}
+            <Link to="/Categories" className="ml-5">
+              Categories
+            </Link>
+            <Link to="/Contact" className="ml-5">
+              Contact
+            </Link>
+            <Link to="/Help" className="ml-5">
+              Help
+            </Link>
+            
+          </div>
 
-         <div className="flex items-center ml-20">
-        <Link to={"/Cart"} className="menu-item">
-          Cart
-        </Link>
-        <button>Join Blue Space</button>
+          <div className="flex ml-10 items-center">
+            <Link to="/Cart" className="bg-white px-4 py-3">
+              Cart
+            </Link>
+            <div className="w-full">
+               <button className="ml-5 bg-black text-white text-xs w-full px-7 py-2">
+              Join Blue Space
+            </button>
+            </div>
+           
+          </div>
+        </div>
       </div>
-        </div>
-        
-      </div>
-
-      
     </div>
   );
 }
