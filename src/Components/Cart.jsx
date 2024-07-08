d239a7605cced8c658898ec76201be8c079a6851
Amelss <ameleyka@gmail.com>
2024-07-08 15:20:44 +0100

    editing cart page

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -5,6 +5,7 @@ import ProductCard from "./ProductCard";
 export default function Cart() {
 
   const [similarProducts, setSimilarProducts] = useState([]);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     const fetchData = () => {
@@ -17,50 +18,85 @@ export default function Cart() {
     fetchData();
   }, []); 
 
+const handleQuantityChange = (amount) => {
+  setQuantity((prevQuantity) => {
+    const newQuantity = prevQuantity + amount;
+    return newQuantity < 1 ? 1 : newQuantity;
+  });
+};
+  
   return (
     <div>
-      <div className="block xl:flex items-center justify-between">
+      <div className="block xl:flex items-center justify-between mt-6">
         <div>
-          <p>Home \ Cart</p>
-          <div className="flex items-center">
-            <img src="" alt="" />
-            <div>
-              <h1>Albus Medium - $650</h1>
-              <p>
+          <p className="text-xs mb-3">
+            <span className="text-gray-300">Home </span>\ Cart
+          </p>
+          <div className="flex items-center justify-between mb-10">
+            <img
+              src="/products/black-office-chairs-3.jpeg"
+              alt="Product image"
+              className="w-20"
+            />
+            <div className="ml-10">
+              <h1 className="mb-3">Albus Medium - $650</h1>
+              <p className="text-xs xl:text-md">
                 Comfort and functionality combined. this chair promotes the back
                 health an beautifies the office environment
               </p>
             </div>
           </div>
-          <div className="flex items-center justify-between">
-            <p>Quantity</p>
-            <button>Remove</button>
+          <div className="flex items-center justify-between mb-10">
+            <div className="flex items-center">
+              <p>Quantity</p>
+              <div className="flex items-center ml-3">
+                <button
+                  className="px-2 py-1 bg-gray-200"
+                  onClick={() => handleQuantityChange(-1)}
+                >
+                  -
+                </button>
+                <p className="px-3">{quantity}</p>
+                <button
+                  className="px-2 py-1 bg-gray-200"
+                  onClick={() => handleQuantityChange(1)}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+
+            <button className="bg-black text-white p-4 w-40">Remove</button>
           </div>
         </div>
 
         <div>
-          <div className="bg-white">
-            <h1>Cart Summary</h1>
-            <p>Complete your purchase...</p>
-            <div className="flex items-center justify-between">
-              <p>Subtotal</p>
+          <div className="bg-white p-3">
+            <h1 className="text-xl font-bold mb-3">Cart Summary</h1>
+            <p className="hidden xl:block text-sm text-gray-600 mb-3">Complete your purchase by providing your payment details</p>
+            <div className="flex items-center justify-between text-sm">
+              <p className="text-gray-300 ">Subtotal</p>
               <p>$650</p>
             </div>
-            <div className="flex items-center justify-between">
-              <p>Discount</p>
+            <div className="flex items-center justify-between text-sm">
+              <p className="text-gray-300">Discount</p>
               <p>$0</p>
             </div>
-            <div className="flex items-center justify-between">
-              <p>Total</p>
+            <div className="flex items-center justify-between text-sm">
+              <p className="">Total</p>
               <p>$650</p>
             </div>
-            <button>Checkout</button>
+            <div className="text-center mt-7">
+              <button className="bg-black text-white p-4 w-full ">
+                Checkout
+              </button>
+            </div>
           </div>
         </div>
       </div>
 
-      <h1>Similar to this Product</h1>
-      <div className="block xl:flex items-center justify-center">
+      <h1 className="text-lg xl:text-3xl mt-10 font-bold">Similar to this Product</h1>
+      <div className="grid xl:grid-cols-3 gap-3 mt-5">
         {similarProducts.slice(1, 4).map((item, index) => (
           <ProductCard key={index} item={item} />
         ))}
