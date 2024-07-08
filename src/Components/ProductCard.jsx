c5e280cad36b81af31958f218a5152d74d5c159a
Amelss <ameleyka@gmail.com>
2024-07-08 20:18:46 +0100

    adjusting hr line

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -5,9 +5,9 @@ export default function ProductCard({ item }) {
     <div className="mt-1 bg-white pt-5">
       <div className="">
         <img src={item.productImage} alt="Product image" className="w-full m-auto pb-2" />
-        <hr className="w-80 xl:w-[600px] m-auto xl:px-4"/>
+        <hr className="w-80 xl:w-[400px] m-auto xl:px-4"/>
         <div className="flex items-center justify-between mt-3 px-2">
-          <h1 className="mt-1">{item.productName}</h1>
+          <h1 className="mt-1 ">{item.productName}</h1>
           <img src="/cart-icon.png" alt="Cart Icon" className="w-7" />
         </div>
 
